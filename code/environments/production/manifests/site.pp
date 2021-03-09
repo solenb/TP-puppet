dc98e7a6ebf33773bca215cbb9079ed4d0c98760
root <root@puppetmaster.localdomain>
2021-03-09 12:00:04 +0100

    first commit

---
+++ code/environments/production/manifests/site.pp
@@ -0,0 +1,4 @@
+node 'rogue1' {
+        notify { 'HelloWorld!': }
+	include sudo
+}
---
+++ code/environments/production/modules/sudo/manifests/init.pp
@@ -0,0 +1,11 @@
+class sudo {
+        package { 'sudo':
+                ensure => 'installed',
+        }
+        file { '/etc/sudoers':
+                ensure  => present,
+                owner   => 'root',
+                group   => 'root',
+                mode    => '0440',
+                source  => 'puppet:///modules/sudo/sudoers',        }
+}
