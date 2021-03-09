1e99fb9d6d15bcda36590a3e7025083a9ea11967
root <root@puppetmaster.localdomain>
2021-03-09 16:36:54 +0100

    working

--- code/environments/production/modules/ssh/manifests/init.pp
+++ code/environments/production/modules/ssh/manifests/init.pp
@@ -2,24 +2,32 @@ class ssh {
         package { 'openssh-server':
                 ensure => 'installed',
         }
-$os = $facts['os']['family'] ? {
-  'RedHat' 		=> 'sshd',
-  /(Debian|Ubuntu)/ 	=> 'ssh',
-  default               => 'root',
-}
 
-service { '$os':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['openssh-server'],
+	case $::osfamily {
+ 		'Debian': { $sshd_service = 'ssh' }
+        	'RedHat': { $sshd_service = 'sshd' }
+	 	default: {fail("Invalid osfamily: ${::osfamily}")}
+    	}
+
+
+	service { 'ssh':
+    		name => $sshd_service,
+        	ensure => running,
+        	enable => true,
+        	hasstatus => true,
+        	hasrestart => true,
+    		require => Package['openssh-server'],
 	}
-file { '/etc/ssh/sshd_config':
-    notify  => Service['$os'],
-    mode    => '0600',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['openssh-server'],
-    content  => 'template("ssh/sshd_config.erb")',
-  }
+
+
+
+	file { '/etc/ssh/sshd_config':
+    		notify  => Service['$os'],
+    		mode    => '0600',
+    		owner   => 'root',
+    		group   => 'root',
+    		require => Package['openssh-server'],
+    	content  => 'template("ssh/sshd_config.erb")',
+  	}
 
 }
